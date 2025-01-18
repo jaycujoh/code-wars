623a7c19b3677bf2f31ce11a11ae71f50c10deea
jaycujoh <jamie.bellpl@gmail.com>
2025-01-18 12:37:46 +0100

    changing the description again

--- javascript/8kyu/removeVowels.js
+++ javascript/8kyu/removeVowels.js
@@ -7,11 +7,12 @@ function disemvowel(str) {
     const vowels = ['a', 'e', 'i', 'o', 'u']
     
     for (i = 0; i > str.length; i++) {
-      if (str.charAt[i] === vowels[i]) {
+      if (str.charAt[i] === vowels.length) {
         str.chatAt(i).pop
       }
     }
+
+    return str;
 }
     
-    return str;
 

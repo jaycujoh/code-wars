cbf867da028f6d775916bbbc97bc3f39138b3893
jaycujoh <jamie.bellpl@gmail.com>
2025-01-18 13:34:19 +0100

    this is a test

--- javascript/8kyu/removeVowels.js
+++ javascript/8kyu/removeVowels.js
@@ -1,18 +1,15 @@
-// code doesn't work just testing github with it
-
-
-
 function disemvowel(str) {
   
-    const vowels = ['a', 'e', 'i', 'o', 'u']
-    
+    const vowels = ['a', 'e', 'i', 'o', 'u'];
+  
     for (i = 0; i > str.length; i++) {
-      if (str.charAt[i] === vowels.length) {
-        str.chatAt(i).pop
-      }
-    }
+      if (str.charAt(i) === vowels(i)) {
+            str.replace(str.charAt(i), '')
+        }
 
     return str;
-}
     
+    }
+  }
 
+  console.log(disemvowel("This website is for losers LOL!"));

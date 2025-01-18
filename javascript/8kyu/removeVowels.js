2813cb7dcfc5982bce23a9154108ccd1fcadb330
jaycujoh <jamie.bellpl@gmail.com>
2025-01-18 13:47:52 +0100

    removing vowels from a string

--- javascript/8kyu/removeVowels.js
+++ javascript/8kyu/removeVowels.js
@@ -1,15 +1,21 @@
 function disemvowel(str) {
   
-    const vowels = ['a', 'e', 'i', 'o', 'u'];
-  
-    for (i = 0; i > str.length; i++) {
-      if (str.charAt(i) === vowels(i)) {
-            str.replace(str.charAt(i), '')
-        }
+    const vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
 
-    return str;
+    // Start with an empty string to store the result
+    let noVowels = '';
+
+    // Loop through each character in the string
+    for (let i = 0; i < str.length; i++) {
+        // Check if the character is NOT a vowel
+        if (!vowels.includes(str[i])) { 
+            // Add the character to the result if it's not a vowel
+            noVowels += str[i]; 
+        }
+    }
+
+    return noVowels;
     
     }
-  }
 
   console.log(disemvowel("This website is for losers LOL!"));

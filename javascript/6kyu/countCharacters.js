0792cdb447a030cdb53c017764ee61897277674c
jaycujoh <jamie.bellpl@gmail.com>
2025-01-22 07:56:43 +0100

    counting how many times a character appears in a string

---
+++ javascript/6kyu/countCharacters.js
@@ -0,0 +1,17 @@
+// The main idea is to count all the occurring characters in a string. If you have a string like aba, then the result should be {'a': 2, 'b': 1}.
+
+// What if the string is empty? Then the result should be empty object literal, {}.
+
+function count(string) {
+
+    string = string.split('');
+    stringArray = {};
+
+    string.forEach(num => {
+        stringArray[num] = (stringArray[num] || 0) + 1;
+    })
+    return stringArray;
+  }
+
+  string = "Hello aba";
+  console.log(count(string));

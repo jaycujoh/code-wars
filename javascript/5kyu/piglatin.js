d8abef15fd7bdb7d94af42025b19f8911e46c709
jaycujoh <jamie.bellpl@gmail.com>
2025-01-26 18:51:32 +0100

    changing a sentence to pig latin (botched version)

--- javascript/5kyu/piglatin.js
+++ javascript/5kyu/piglatin.js
@@ -5,5 +5,26 @@
 // pigIt('Hello world !');     // elloHay orldway !
 
 function pigIt(str){
-    
-  }
+
+  //assign the first letter to the end of a word then +'ay' to the word.
+
+  const newSentence = str.split(' ');
+
+  let splitWord = '';
+  let pigLatin = '';
+
+  for (i = 0; i < newSentence.length; i++) {
+    //split the array value at [i] into a seperate array of each letter
+      splitWord = newSentence[i].split('');
+      //go through the new letter array 
+      for (let j = 0; j < 1; j++) {
+          //grab the first indexed letter of the array and add it to end instead
+            let firstIndex = splitWord.shift();
+            splitWord.push(firstIndex);
+          //join the letters together and add the new word to a new array with 'ay' added on.
+            pigLatin += splitWord.join('') + 'ay ';
+            }
+          }
+          return pigLatin.trim();
+}
+  console.log(pigIt('Pig latin is cool'));
